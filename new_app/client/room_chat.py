a54219ddb0aa99bff5cb95920cb95d453b8f6ba3
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-06-10 20:11:03 +0700

    create sender interface

--- new_app/client/room_chat.py
+++ new_app/client/room_chat.py
@@ -1,4 +1,14 @@
 import flet as ft
+import time
+
+list_chat = [
+  "Halo",
+  "Halo juga",
+  "Apa kabar?",
+  "Baik, kamu?",
+  "Juga baik",
+]
+
 
 def main(page: ft.Page):
   page.horizontal_alignment = "stretch"
@@ -6,15 +16,76 @@ def main(page: ft.Page):
   page.scroll="auto"
   
   all_messages = ft.Column(scroll="auto")
+  
+  def loop_chat_dummy():
+    for index, message in enumerate(list_chat):
+        all_messages.controls.append(
+          # create user chat
+            ft.Row([
+                ft.Container(
+                    padding=20,
+                    border_radius=20,
+                    bgcolor="blue200" if index % 2 == 0 else "green200",
+                    content=ft.Column([
+                        ft.Text(your_username.value, size=18, weight="bold") if index % 2 == 0 else ft.Text(
+                            "Anyone", size=18, weight="bold"),
+                        ft.Text(message, size=22)
+                    ])
+                )
+            ], alignment="end" if index % 2 == 0 else "start")
+        )
+
+        page_layout.visible = True
+        page.update()
+        time.sleep(0.5)
+  
+  # Show all messages
+  def get_message(e):
+    if not your_username.value:
+      your_username.error_text = "Please enter your name"
+      page.update()
+    elif not your_password.value:
+      your_password.error_text = "Please enter your password"
+      page.update()
+    elif your_username.value != "messi" or your_password.value != "surabaya":
+      your_username.error_text = "Your account is not registered"
+      your_password.error_text = "Your account is not registered"
+      page.update()
+    else:
+      page.dialog.open = False
+      all_messages.controls.clear()
+      loop_chat_dummy()
     
-    # menambah list chat
+  # ========menambah list chat
   def send_message_click(e):
     if chat_field.value:
       all_messages.controls.append(ft.Text(chat_field.value))
       chat_field.value = ""
       page.update()
   
-  # Chat messages field
+  # =============A dialog asking for a user display name
+  your_username = ft.TextField(
+    label="Enter your username",
+    autofocus=True,
+    on_submit=get_message,
+  )
+  
+  your_password = ft.TextField(
+    label="Enter your password",
+    autofocus=True,
+    on_submit=get_message,
+  )
+  
+  page.dialog = ft.AlertDialog(
+    open=True,
+    modal=True,
+    title=ft.Text("Welcome!"),
+    content=ft.Column([your_username, your_password],tight=True),
+    actions=[ft.ElevatedButton(text="Join chat", on_click=get_message)],
+    actions_alignment="end",
+    )# A dialog asking for a user display name
+
+  # ================Chat messages field
   chat_field = ft.TextField(
         label="Write a message...",
         autofocus=True,
@@ -40,6 +111,7 @@ def main(page: ft.Page):
   page_layout = ft.Column([
     ft.Container(
       height=500,
+      padding=20,
       content=ft.Column([
         all_messages,
       ], scroll="auto")
@@ -47,8 +119,7 @@ def main(page: ft.Page):
       chat_cont
   ])          
     
-    # Add everything to the page
-  
+  # =========Add everything to the page
   page.add(page_layout)
 
 ft.app(target=main)
